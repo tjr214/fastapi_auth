e1db008d7c2be60076030487eab223adb64250df
Timothy Roff <tjr214@mailbox.org>
2024-02-09 23:08:46 -0500

    Begin logging system implementation
    Basic logger implemented as middleware. Can currently:
    Log to stdout (or stderr, etc.)
    Log to a file.
    Log to papertrail.
    
    Just need to make a normal logger for non-middleware related stuff.

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 from rich import print
 
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.base import BaseHTTPMiddleware
 
+from auth import auth_router
 
 from routes.todo_routes import todo_router
 from routes.user_routes import user_router
 from routes.jinja_routes import jinja_router
-from auth import auth_router
 
-# Define our app
+from middleware.logger import log_middleware, logger
+
+
+# Define our app and add any relevant middleware
 app = FastAPI()
+app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
 
 # Set the StaticFiles location for our assets (like CSS)
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
-
 
 # Add our routes
 routers = [
@@ -35,5 +39,6 @@ async def root():
 # Start the server with uvicorn
 if __name__ == "__main__":
     import uvicorn
-    print("[green]STARTING UP...![/green]")
+    # print("[green]STARTING UP...![/green]")
+    logger.info("Starting up!")
     uvicorn.run("server:app", host="0.0.0.0", port=8181, reload=True)
